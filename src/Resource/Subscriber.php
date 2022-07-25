2080b411271afbd88917b05040c22183f0dc392f
FMP-Dev <109966286+FMP-Dev@users.noreply.github.com>
2022-07-26 00:06:36 +0300

    Add more methods
    findByEmail GET => PATCH
    + findByID
    + addTag

--- src/Resource/Subscriber.php
+++ src/Resource/Subscriber.php
@@ -12,7 +12,12 @@ class Subscriber extends Base {
 
     public function findByEmail($email)
     {
-        return $this->makeRequest('email/' . $email, 'GET');
+        return $this->makeRequest('email/' . $email, 'PATCH');
+    }
+
+    public function findByID($email)
+    {
+        return $this->makeRequest('id/' . $email, 'PATCH');
     }
 
     public function subscribe($uid)
@@ -23,5 +28,10 @@ class Subscriber extends Base {
     public function unsubscribe($uid)
     {
         return $this->makeRequest($uid . '/unsubscribe', 'PATCH');
+    }
+
+    public function addTag($uid)
+    {
+        return $this->makeRequest($uid . '/add-tag', 'POST');
     }
 }
